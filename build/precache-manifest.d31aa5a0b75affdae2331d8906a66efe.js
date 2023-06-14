076e5d9f40092d6495583f142fae1c6bc729b3d7
tasharry-max <nataly.rudnitskaya@gmail.com>
2023-06-14 21:24:37 +0100

    edits

---
+++ build/precache-manifest.d31aa5a0b75affdae2331d8906a66efe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94758d771c5c9f8b83e4",
+    "url": "/portfolio.github.io/static/js/main.590e454b.chunk.js"
+  },
+  {
+    "revision": "e1c6b5e5614769fee81f",
+    "url": "/portfolio.github.io/static/js/runtime~main.f2302257.js"
+  },
+  {
+    "revision": "635e5cb36f1a67069f07",
+    "url": "/portfolio.github.io/static/css/2.461d80ea.chunk.css"
+  },
+  {
+    "revision": "635e5cb36f1a67069f07",
+    "url": "/portfolio.github.io/static/js/2.035e0f06.chunk.js"
+  },
+  {
+    "revision": "e3c778f14745d79ee627fb833a474e8f",
+    "url": "/portfolio.github.io/static/media/profile-img.e3c778f1.gif"
+  },
+  {
+    "revision": "20af9f2c2cc243bdbf8230bd772cd8b3",
+    "url": "/portfolio.github.io/static/media/spaceman.20af9f2c.png"
+  },
+  {
+    "revision": "d4cfac1a114acd26fb4e58a791cdbd4c",
+    "url": "/portfolio.github.io/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5214889798e30f2600eb1603a0925e9f.js"
+  "/portfolio.github.io/precache-manifest.d31aa5a0b75affdae2331d8906a66efe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
